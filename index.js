bc32000c1416286245a9c19af4bb48744965d88b
Marlon20-jar <marlon.jair@outlook.com>
2021-03-11 19:28:29 -0600

    inicial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+/*
+verbos HTTP
+GET
+POST
+PATCH
+PUT
+DELETE
+*/
+
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Hola Mundo");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
