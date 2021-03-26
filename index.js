10dc79f39c3f8e2c0cf5764602a5222d6e66a5dc
Marlon20-jar <marlon.jair@outlook.com>
2021-03-25 20:15:31 -0600

    inicial commmit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const bodyParser = require( 'body-parser' );
 const morgan = require('morgan')
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon');
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 GET- Obtener recursos
@@ -18,10 +17,14 @@ DELETE - Borrar un recurso
 */ 
 
 app.get("/", (req, res, next) => {
-   return res.status(200).send("Bievenido al Pokedex");
+   return res.status(200).json({ code: 1, message: "Bienvenido al pokédex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: "URL no encontrada" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
